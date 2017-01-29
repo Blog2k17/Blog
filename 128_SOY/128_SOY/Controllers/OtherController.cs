bbe60b90176ec3c587ff13555247edd6418c3b70
Bogdan Alov <alovbg21@abv.bg>
2017-01-29 14:53:35 +0200

    added OtherController.cs;

---
+++ 128_SOY/128_SOY/Controllers/OtherController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _128_SOY.Controllers
+{
+    public class OtherController : Controller
+    {
+        // GET: Other
+        public ActionResult Kontakti()
+        {
+            return View();
+        }
+
+        public ActionResult ZaZrelostnici()
+        {
+            return View();
+        }
+        public ActionResult Aktualno()
+        {
+            return View();
+        }
+        public ActionResult Galeriq()
+        {
+            return View();
+        }
+
+        public ActionResult VajniVryzki()
+        {
+            return View();
+        }
+    }
+}
---
+++ 128_SOY/128_SOY/Controllers/PosledniNoviniController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _128_SOY.Controllers
+{
+    public class PosledniNoviniController : Controller
+    {
+        // GET: PosledniNovini
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EseAngliiskiEzikDubai()
+        {
+            return View();
+        }
+
+        public ActionResult ZaednoMojemPoveche()
+        {
+            return View();
+        }
+
+        public ActionResult KlasniStaiObnovqvane()
+        {
+            return View();
+        }
+        public ActionResult ZlatenMedal()
+        {
+            return View();
+        }
+        public ActionResult ObryshtenieNaGnPlachkov()
+        {
+            return View();
+        }
+
+        public ActionResult ZashtitaNaInformaciqta()
+        {
+            return View();
+        }
+
+        public ActionResult ReportajNaNova()
+        {
+            return View();
+        }
+
+        public ActionResult Nagrada()
+        {
+            return View();
+        }
+    }
+}
